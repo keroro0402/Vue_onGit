49a08caf39661df3e28ea1144889c4b95d8fd52b
keroro0402 <otsuyu0402@yahoo.co.jp>
2023-08-07 06:50:50 +0900

    ディレクティブ

---
+++ dirctive1/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref, computed, reactive } from 'vue'
+
+interface Member {
+  name: string
+  age: number
+  sex: string
+}
+
+const people = {
+  name: '斉藤ななみ',
+  age: 32,
+  sex: '女'
+}
+
+const nanami: Member = reactive(people)
+</script>
+<template>
+  <p>{{ nanami.name }}／{{ nanami.age }}／{{ nanami.sex }}</p>
+  <p><img src="@/assets/6.jpeg" /></p>
+  <br />
+  <p><img src="@/assets/Piyoko.png" /></p>
+</template>
