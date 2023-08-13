71e1ca0bc265d768cb909f8bc6c72d8ab2224d2c
keroro0402 <otsuyu0402@yahoo.co.jp>
2023-08-13 21:46:48 +0900

    コミット

---
+++ directive-on/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const result = ref('まだです')
+let htmlStr = ref()
+let count = ref(1)
+const onclick = (): void => {
+  if (count.value > 4) {
+    count.value = 0
+  }
+  count.value++
+  htmlStr.value = "<img src='./src/assets/item" + count.value + ".jpg'>"
+  result.value = ''
+}
+const onchange = (): void => {
+  console.log('sa')
+}
+</script>
+<template>
+  <section>
+    <p>
+      <input v-on:click="onclick" type="button" value="クリック1" />
+      <input v-on:change="onchange" value="10" type="number" />
+    </p>
+    <p><span v-html="htmlStr"></span>{{ result }}</p>
+  </section>
+</template>
