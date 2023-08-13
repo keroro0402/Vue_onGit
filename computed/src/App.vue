46a4d9d1033bcc413c6462bd251cb7e0958a427f
keroro0402 <otsuyu0402@yahoo.co.jp>
2023-08-13 14:08:59 +0900

    コミット

---
+++ computed/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+const randomInit = Math.random()
+const radiusInit = Math.floor(randomInit * 10)
+let radius = ref(radiusInit)
+let PI = Math.floor(Math.PI * 100) / 100
+const f = () => {
+  radius.value = Math.floor(Math.random() * 10 + 4)
+}
+setInterval(f, 1000)
+
+const area = computed((): number => {
+  return radius.value * radius.value * PI
+})
+</script>
+<template>
+  <p>半径：{{ radius }}、円周率：{{ PI }}</p>
+  <p>の円の面積は{{ area }}です</p>
+</template>
