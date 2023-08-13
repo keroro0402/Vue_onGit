1299db288b068df1b2b57b8ee5af217b91cb060d
keroro0402 <otsuyu0402@yahoo.co.jp>
2023-08-13 13:22:41 +0900

    コミット

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+//import { ref } from 'vue'
+//import {ref} from "vue"
 
-const name = ref('こんばんは')
+const name1 = ref('斉藤ななみ') // ref関数：テンプレート変数
+const name2 = ref('佐々木希')
 </script>
-<template></template>
+<template>
+  <!--マスタッシュ構文-->
+  <p>{{ name1 }}</p>
+  <p>{{ name2 }}</p>
+</template>
