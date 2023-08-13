36aa9aa6dbbf2031672d53a13baa1fad8eb6a8a7
keroro0402 <otsuyu0402@yahoo.co.jp>
2023-08-13 15:25:37 +0900

    コミット

--- reactive-function/src/App.vue
+++ reactive-function/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { ref, reactive } from 'vue'
 
 const personInit = {
   name: '斉藤ななみ',
@@ -11,8 +11,14 @@ const personInit = {
 const person = reactive(personInit) // オブジェクトをリアクティブにする時はreactive()メソッドで一気にリアクティブにできるので使うべし！！
 
 person.sayHello()
+
+const img = ref('/src/assets/item1.jpg')
 </script>
 <template>
+  <img v-bind:src="img" />
+  <p>※※※※※※※※※※※※※※</p>
   <p>私の名前は {{ person['name'] }} です。</p>
+  <p><img src="./assets/img/item2.jpg" /></p>
   <p>年齢は {{ person.age }} 歳です。</p>
+  <p><img src="./assets/item1.jpg" /></p>
 </template>
