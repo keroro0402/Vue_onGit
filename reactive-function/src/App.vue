64ecad26aaad4769f3d4cb41450fef86f922d413
keroro0402 <otsuyu0402@yahoo.co.jp>
2023-08-13 14:56:05 +0900

    最初のコミット

---
+++ reactive-function/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const personInit = {
+  name: '斉藤ななみ',
+  age: 32,
+  sayHello: function () {
+    console.log(`${this.name}です`)
+  }
+}
+const person = reactive(personInit) // オブジェクトをリアクティブにする時はreactive()メソッドで一気にリアクティブにできるので使うべし！！
+
+person.sayHello()
+</script>
+<template>
+  <p>私の名前は {{ person['name'] }} です。</p>
+  <p>年齢は {{ person.age }} 歳です。</p>
+</template>
