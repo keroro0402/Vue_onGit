d36807e3bf5c324d6d709cc622cb16828cd67f9a
keroro0402 <otsuyu0402@yahoo.co.jp>
2023-08-13 22:38:29 +0900

    コミット

---
+++ directive-on-img/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const widthInit = '100'
+let width = ref(widthInit)
+
+const onchange = (event: Event): void => {
+  const el = event.target as HTMLInputElement
+  width.value = el.value
+}
+</script>
+<template>
+  <p><input type="number" v-on:change="onchange" v-bind:value="width" /></p>
+  <p><img v-bind:width="width" src="./assets/item1.jpg" /></p>
+</template>
